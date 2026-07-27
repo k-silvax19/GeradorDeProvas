0a6e14a2424d7778b86a2610a4d83a785c7ae7ea
Kauan Silva <kauanmacedo338@gmail.com>
2026-07-27 10:43:53 -0300

    adiciona migrations e repositorio

--- src/GeradorDeProvas.Dominio/Modulos/ModuloMateria/Materia.cs
+++ src/GeradorDeProvas.Dominio/Modulos/ModuloMateria/Materia.cs
@@ -1,6 +1,7 @@
 using GeradorDeProvas.Dominio.Compartilhado;
 using GeradorDeProvas.Dominio.Compartilhado.Identity;
 using GeradorDeProvas.Dominio.Modulos.ModuloDisciplina;
+using GeradorDeProvas.Dominio.Modulos.ModuloProva;
 using GeradorDeProvas.Dominio.Modulos.ModuloQuestao;
 
 namespace GeradorDeProvas.Dominio.Modulos.ModuloMateria;
@@ -12,6 +13,7 @@ public class Materia : EntidadeBase<Materia>, IEntidadeDoUsuario
     public Disciplina Disciplina { get; set; } = null!;
     public List<Questao> Questoes { get; set; } = [];
     public Guid UserId { get; set; }
+    public List <Prova> Provas { get; set; } = [];
 
     public Materia()
     {
--- src/GeradorDeProvas.Infra/Compartilhado/Orm/GeradorDeProvasDbContext.cs
+++ src/GeradorDeProvas.Infra/Compartilhado/Orm/GeradorDeProvasDbContext.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using GeradorDeProvas.Dominio.Compartilhado.Identity;
 using GeradorDeProvas.Dominio.Modulos.ModuloDisciplina;
 using GeradorDeProvas.Dominio.Modulos.ModuloMateria;
+using GeradorDeProvas.Dominio.Modulos.ModuloProva;
 using GeradorDeProvas.Dominio.Modulos.ModuloQuestao;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -42,6 +43,9 @@ public sealed class GeradorDeProvasDbContext(
 
             modelBuilder.Entity<Alternativa>()
                 .HasQueryFilter(a => a.UserId == userProvider.Id);
+
+            modelBuilder.Entity<Prova>()
+                .HasQueryFilter(p => p.UserId == userProvider.Id);
         }
     }
 
